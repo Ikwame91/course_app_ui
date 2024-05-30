3e78626ab567c717deebad42822cad7d691efa9a
Kwame Agyapong <Kwameagyapong91@gmail.com>
2024-05-30 21:41:04 +0000

    liveCourses

---
+++ lib/models/course/live_course.dart
@@ -0,0 +1,46 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter_english_course/models/course/course.dart';
+import 'package:flutter_english_course/models/course/teacher.dart';
+
+class LiveCourse extends Course {
+  final String liveStreamingUrl;
+  final String liveDuration;
+  final DateTime liveTime;
+
+  LiveCourse(
+      {required super.id,
+      required super.title,
+      required super.description,
+      required super.level,
+      required super.imageUrl,
+      required super.teacher,
+      required this.liveStreamingUrl,
+      required this.liveDuration,
+      required this.liveTime,
+      super.countStudents = 0,
+      required super.createdAt});
+
+  // Map<String, dynamic> toMap() {
+  //   return <String, dynamic>{
+  //     'liveStreamingUrl': liveStreamingUrl,
+  //     'liveDuration': liveDuration,
+  //     'liveTime': liveTime.millisecondsSinceEpoch,
+  //   };
+  // }
+
+  factory LiveCourse.fromMap(Map<String, dynamic> json) {
+    return LiveCourse(
+      id: int.tryParse(json["id"].toString()) ?? 0,
+      title: json["title"],
+      description: json["description"],
+      level: json["level"],
+      imageUrl: json["imageUrl"],
+      teacher: Teacher.fromJson(json["teacher"]),
+      liveTime: DateTime.parse(json["liveTime"].toString()),
+      liveStreamingUrl: json["liveStreamingUrl"],
+      liveDuration: json["liveDuration"],
+      countStudents: int.tryParse(json["countStudents"].toString()) ?? 0,
+      createdAt: DateTime.parse(json["createdAt"].toString()).toLocal(),
+    );
+  }
+}
--- lib/modules/menus/live_courses_view.dart
+++ lib/modules/menus/live_courses_view.dart
@@ -1,17 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_english_course/components/components.dart';
 import 'package:flutter_english_course/cores/cores.dart';
 
-class LiveCoursesView extends StatelessWidget {
+class LiveCoursesView extends StatefulWidget {
   static const String routeName = '/live-courses';
 
   const LiveCoursesView({super.key});
 
   @override
+  State<LiveCoursesView> createState() => _LiveCoursesViewState();
+}
+
+class _LiveCoursesViewState extends State<LiveCoursesView> {
+  final liveCourses = <LiveCourse>[];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "Live Courses Screen",
-        style: context.theme.textTheme.titleLarge,
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: context.theme.scaffoldBackgroundColor,
+        centerTitle: false,
+        title: Text(
+          "Live Courses",
+          style: context.theme.textTheme.titleLarge,
+        ),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.all(14.0),
+            child: Icon(
+              AppIcons.calendar,
+              size: 26,
+              color: Colors.grey,
+            ),
+          )
+        ],
+      ),
+      body: AppPullRefresh(
+        onRefresh: () async {
+          //load data here
+        },
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 18)
+                    .copyWith(bottom: 10),
+                child: Text(
+                  "Wonderful Live Courses Interact with Teachers",
+                  style: p15.grey,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
